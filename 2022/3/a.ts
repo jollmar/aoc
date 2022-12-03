2ce80f76952e96f58fdb920fb101cf238b230713
Johan Ljung <jollelj@hotmail.com>
2022-12-03 06:30:37 +0100

    day 3

---
+++ 2022/3/a.ts
@@ -0,0 +1,79 @@
+const input = Deno.readTextFileSync("input.txt");
+const chars = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+const rows = input.split("\n");
+
+const data = rows
+  .reduce((carry: string[], row) => {
+    const a = row.split("").slice(0, row.split("").length / 2);
+    const b = row
+      .split("")
+      .slice(row.split("").length / 2, row.split("").length);
+
+    const dupe = a.find((char) => b.includes(char));
+    if (dupe) {
+      carry.push(dupe);
+    }
+
+    return carry;
+  }, [])
+  .sort((a, b) => a.localeCompare(b))
+  .reduce((carry, char) => {
+    carry += chars.indexOf(char) + 1;
+    return carry;
+  }, 0);
+
+console.log(data);
---
+++ 2022/3/b.ts
@@ -0,0 +1,80 @@
+const input = Deno.readTextFileSync("input.txt");
+const chars = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+const rows = input.split("\n");
+
+const data = rows
+  .reduce((carry: Array<string[]>, value, index) => {
+    if (index % 3 === 0) {
+      carry.push([]);
+    }
+
+    carry[carry.length - 1].push(value);
+    return carry;
+  }, [])
+  .reduce((carry, arrays) => {
+    const [a, b, c] = arrays.map((arr) => arr.split(""));
+
+    const dupe = a.find((char) => b.includes(char) && c.includes(char));
+
+    if (dupe) {
+      carry += chars.indexOf(dupe) + 1;
+    }
+
+    return carry;
+  }, 0);
+
+console.log(data);
